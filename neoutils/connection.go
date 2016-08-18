dafe38c5cf6e628c5034044071417a4b95232825
Martin Garton <martin.garton@ft.com>
2016-08-18 18:24:30 +0100

    Added Connect()
    Added a Connect() function that should serve as the main entry point to
    this package in future.  This deprecates various other types that are
    currently exposed in favour of a single function with config options.
    This will allow for greater flexibility in future once applcations are
    all using this approach.  In particular, auto-reconnection and dealing
    gracefully with an unavailable database will be possible.

---
+++ neoutils/connection.go
@@ -0,0 +1,99 @@
+package neoutils
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/jmcvetta/neoism"
+)
+
+type ConnectionConfig struct {
+	// BatchSize controls how and whether to batch multiple requests to
+	// CypherQuery into a single batch. BatchSize 0 disables this behaviour.
+	// Values >0 indicate the largest preferred batch size.  Actual sizes
+	// may be larger because values from a single call will never be split.
+	BatchSize     int
+	Transactional bool
+	HTTPClient    *http.Client
+}
+
+func DefaultConfig() *ConnectionConfig {
+	return &ConnectionConfig{
+		BatchSize:     1024,
+		Transactional: true,
+		HTTPClient: &http.Client{
+			Transport: &http.Transport{
+				MaxIdleConnsPerHost: 100,
+			},
+		},
+	}
+}
+
+func Connect(neoURL string, conf *ConnectionConfig) (NeoConnection, error) {
+	if conf == nil {
+		conf = DefaultConfig()
+	}
+
+	db, err := neoism.Connect(neoURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if conf.HTTPClient != nil {
+		db.Session.Client = conf.HTTPClient
+	}
+
+	var cr CypherRunner = db
+	if conf.Transactional {
+		cr = TransactionalCypherRunner{db}
+	} else {
+		cr = db
+	}
+
+	if conf.BatchSize > 0 {
+		cr = NewBatchCypherRunner(cr, conf.BatchSize)
+	}
+
+	ie := &defaultIndexEnsurer{db}
+
+	return &DefaultNeoConnection{neoURL, cr, ie, db}, nil
+}
+
+type DefaultNeoConnection struct {
+	dbURL string
+	cr    CypherRunner
+	ie    IndexEnsurer
+
+	db *neoism.Database
+}
+
+func (c *DefaultNeoConnection) CypherBatch(cypher []*neoism.CypherQuery) error {
+	return c.cr.CypherBatch(cypher)
+
+}
+
+func (c *DefaultNeoConnection) EnsureConstraints(constraints map[string]string) error {
+	return c.ie.EnsureConstraints(constraints)
+}
+
+func (c *DefaultNeoConnection) EnsureIndexes(indexes map[string]string) error {
+	return c.ie.EnsureIndexes(indexes)
+}
+
+func (c *DefaultNeoConnection) String() string {
+	return fmt.Sprintf("DefaultNeoConnection(%s)", c.dbURL)
+}
+
+var _ NeoConnection = (*DefaultNeoConnection)(nil) //{}
+
+type defaultIndexEnsurer struct {
+	db *neoism.Database
+}
+
+func (ie *defaultIndexEnsurer) EnsureIndexes(indexes map[string]string) error {
+	return EnsureIndexes(ie.db, indexes)
+}
+
+func (ie *defaultIndexEnsurer) EnsureConstraints(constraints map[string]string) error {
+	return EnsureConstraints(ie.db, constraints)
+}
