0b0369ae1b7a3962dd732b5aaa7e5eeb18cc28f4
Martin Garton <martin.garton@ft.com>
2016-08-19 13:43:33 +0100

    Remove NeoConnectionLegacy
    Replace NeoConnectionLegacy with a function that extracts the underlying
    neoism connection for those cases where it is still required.

--- neoutils/connection_compat.go
+++ neoutils/connection_compat.go
@@ -1,84 +1,19 @@
 package neoutils
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/jmcvetta/neoism"
 )
 
-type NeoConnectionLegacy struct {
-	NeoConnection
-	db *neoism.Database
-}
-
-func (ncl *NeoConnectionLegacy) NeoDB() *neoism.Database {
-	return ncl.db
-}
-
-func ConnectLegacy(neoURL string, conf *ConnectionConfig) (*NeoConnectionLegacy, error) {
-	con, err := Connect(neoURL, conf)
-	if err != nil {
-		return nil, err
-	}
-
-	var db *neoism.Database
+func UnderlyingDB(con NeoConnection) *neoism.Database {
+	log.Println("App is using neoism connection directly. Please update when possible to avoid this.")
 
 	switch c := con.(type) {
 	case *DefaultNeoConnection:
-		db = c.db
+		return c.db
 	default:
-		return nil, fmt.Errorf("Unable to create NeoConnectionLegacy for underlying type %T", con)
+		panic("unhandled NeoConnection type")
 	}
 
-	return &NeoConnectionLegacy{con, db}, nil
 }
-
-/*
-type legacyIndexManager struct {
-	db *neoism.Database
-}
-
-func (lim *legacyIndexManager) CreateIndex(label, property string) (*neoism.Index, error) {
-	return lim.db.CreateIndex(label, property)
-}
-
-func (lim *legacyIndexManager) CreateUniqueConstraint(label, property string) (*neoism.UniqueConstraint, error) {
-	return lim.db.CreateUniqueConstraint(
-	panic("write me")
-}
-
-func (lim *legacyIndexManager) Indexes(label string) ([]*neoism.Index, error) {
-	panic("write me")
-}
-
-func (lim *legacyIndexManager) UniqueConstraints(label, property string) ([]*neoism.UniqueConstraint, error) {
-	panic("write me")
-}
-
-type warnIndexManager struct {
-}
-
-func (lim *warnIndexManager) CreateIndex(label, property string) (*neoism.Index, error) {
-	lim.warn()
-	return nil, nil
-}
-
-func (lim *warnIndexManager) CreateUniqueConstraint(label, property string) (*neoism.UniqueConstraint, error) {
-	lim.warn()
-	return nil, nil
-}
-
-func (lim *warnIndexManager) Indexes(label string) ([]*neoism.Index, error) {
-	lim.warn()
-	return []*neoism.Index{}, nil
-}
-
-func (lim *warnIndexManager) UniqueConstraints(label, property string) ([]*neoism.UniqueConstraint, error) {
-	lim.warn()
-	return []*neoism.UniqueConstraint{}, nil
-}
-
-func (lim *warnIndexManager) warn() {
-	log.Print("WARN : Not handling indexes or unique constraints.  Please update app to use neoutils.Connect()")
-}
-*/
